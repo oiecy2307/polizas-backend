280e462878f3a3434bded149bc4bfbebe940a579
DANIEL DANEY LOPEZ MEDINA <13170780@itculiacan.edu.mx>
2018-10-30 22:44:50 -0700

    agregando metodos update, getone y delete

--- src/api/users.js
+++ src/api/users.js
@@ -3,6 +3,8 @@ import { datastore } from '../lib/util';
 
 export default () => {
 	const userRouter = express.Router();
+
+	// GET ALL USERS
 	userRouter.route('/')
 		.get((req, res) => {
 			const query = datastore
@@ -28,6 +30,32 @@ export default () => {
 				});
 		});
 
+	// GET ONE USER
+	userRouter.route('/:id')
+		.get((req, res) => {
+			const userKey = datastore.key(['User', Number(req.params.id)]);
+			const query = datastore
+				.createQuery('User')
+				.filter('__key__', '=', userKey)
+				.limit(1);
+
+				datastore
+					.runQuery(query)
+					.then((response) => {
+						console.log('RESPONSEEEE', response);
+						// The response is an array that contains in [0] the results and in [1] extra information
+						const results = response[0];
+						if (!results.length) res.status(412).send({ message: 'User not found' });
+						const user = results[0];
+						res.status(200).send(user);
+					})
+					.catch(err => {
+						console.error('ERROR:', err);
+						res.status(500).send(err);
+					});
+		});
+
+	// CREATE A NEW USE
 	userRouter.route('/')
 		.post((req, res) => {
 			const { body } = req;
@@ -61,11 +89,68 @@ export default () => {
 					res.status(500).send(err);
 				})
 
+		});
+
+
+	// DELETE AN USER
+	userRouter.route('/:id')
+		.delete((req, res) => {
+			const transaction = datastore.transaction();
+			const userKey = datastore.key(['User', Number(req.params.id)]);
+
+			transaction
+				.run()
+				.then(() => transaction.delete(userKey))
+				.then(() => {
+					return transaction.commit();
+				})
+				.then((result) => {
+					console.log('resultresultresult', result);
+					res.status(200).send({ message: 'User deleted' });
+				})
+				.catch((e) => {
+					console.log(e);
+					res.status(500).send(e);
+					transaction.rollback();
+				});
+
 		})
 
+	// UPDATE AN USER
 	userRouter.route('/:id')
-		.get((req, res) => {
-			res.json({ message: req.params.id });
+		.patch((req, res) => {
+			const { body } = req;
+			if (!body.name) res.status(412).send({
+				message: 'Incomplete data',
+			});
+
+			const transaction = datastore.transaction();
+			const userKey = datastore.key(['User', Number(req.params.id)]);
+			let finalUser = {};
+
+			transaction
+				.run()
+				.then(() => transaction.get(userKey))
+				.then((results) => {
+					console.log('RESULTSSSS', results);
+					const user = results[0];
+					user.name = body.name;
+					transaction.save({
+						key: userKey,
+						data: user,
+					});
+					finalUser = user;
+					return transaction.commit();
+				})
+				.then((result) => {
+					console.log('resultresultresult', result);
+					res.status(200).send(finalUser);
+				})
+				.catch((e) => {
+					console.log(e);
+					res.status(500).send(e);
+					transaction.rollback();
+				});
 		});
 
 	return userRouter;
