962d4e9eec7a451a763c3ac20140fe55c5e23c6f
pedromedina95 <pedro@datank.ai>
2020-01-27 17:37:33 -0700

    escribiendo modelo de ticket, pendiente probar

--- src/models/ticket.js
+++ src/models/ticket.js
@@ -7,6 +7,58 @@ export default (sequelize) => {
       primaryKey: true,
       autoIncrement: true,
     },
+    reporterId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    technicalId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    clientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('new', 'in-progress', 'finished', 'closed', 'cancelled'),
+      allowNull: false,
+    },
+    reportedDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    shortName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    closed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    documentId: {
+      type: DataTypes.STRING,
+    },
+    timeNeeded: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    cost: {
+      type: DataTypes.FLOAT,
+      defaultValue: 0,
+    },
+    solution: {
+      type: DataTypes.TEXT,
+      defaultValue: '',
+      allowNull: false,
+    },
   }, {});
   return Ticket;
 }
