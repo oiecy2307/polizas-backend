d25fa2cefb77a1a2cea19fffde1a9c01b44cbd19
pmedina95 <pmedina.c95@gmail.com>
2020-01-28 23:21:55 -0700

    guardando usuarios con servicio de signup, se implementa jwt, faltan pruebas

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { get } from 'lodash';
 
 export default (sequelize, User) => {
   const router = express.Router();
@@ -20,10 +22,41 @@ export default (sequelize, User) => {
 
   router
     .route('/signup')
-    .post(async (req, res) => {
+    .post(async ({ body }, res) => {
       try {
-        const password = await bcrypt.hash(req.body.password, 10);
-        res.status(200).send(password);
+        const {
+          password,
+          email,
+          username,
+          name,
+          lastname,
+          secondLastName,
+          role,
+        } = body;
+        if (!(password && email && username && name && lastname && role)) {
+          res.status(412).json({ error: true, message: 'Datos incompletos' });
+          return;
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User
+          .build({
+            password: hashedPassword,
+            email,
+            username,
+            name,
+            lastname,
+            secondLastName,
+            role,
+          })
+          .save();
+        // TODO: CHANGE SECRET FOR: process.env.APP_SECRET
+        const token = await jwt.sign({ userId: newUser.id }, 'temporal-pedro');
+        res
+          .status(200)
+          .json({
+            user: newUser,
+            token,
+          });
       } catch (e) {
         res.status(500).send(e);
       }
