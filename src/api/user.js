691ae6551ce04dd935ee1c6950d6e02bd26cfe3d
pedromedina95 <pedro@datank.ai>
2020-01-29 10:31:34 -0700

    ejemplo usando authorization

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { get } from 'lodash';
+import { Op } from 'sequelize';
 
 export default (sequelize, User) => {
   const router = express.Router();
@@ -10,6 +10,31 @@ export default (sequelize, User) => {
     .route('/')
     .get(async (req, res) => {
       try {
+        const users = await User.findAndCountAll();
+        res.status(200).json({
+          data: users,
+          error: false,
+        });
+      } catch (e) {
+        res.status(500).send(e);
+      }
+    });
+
+  router
+    .route('/getAll')
+    .get(async (req, res) => {
+      try {
+        const Authorization = req.headers.authorization;
+        if (!Authorization) {
+          res.status(412).json({ error: true, message: 'Unauthorized' });
+          return;
+        }
+        const token = Authorization.replace('Bearer ', '');
+        // TODO: CHANGE SECRET FOR: process.env.APP_SECRET
+        const { role } = jwt.verify(token, 'temporal-pedro');
+        if (role !== 'admin') {
+          res.status(401).json({ error: true, message: 'Unauthorized' });
+        }
         const users = await User.findAndCountAll();
         res.status(200).json({
           data: users,
@@ -50,7 +75,7 @@ export default (sequelize, User) => {
           })
           .save();
         // TODO: CHANGE SECRET FOR: process.env.APP_SECRET
-        const token = await jwt.sign({ userId: newUser.id }, 'temporal-pedro');
+        const token = await jwt.sign({ userId: newUser.id, role }, 'temporal-pedro');
         res
           .status(200)
           .json({
@@ -58,6 +83,44 @@ export default (sequelize, User) => {
             token,
           });
       } catch (e) {
+        res.status(500).send(e);
+      }
+    });
+
+  router
+    .route('/login')
+    .post(async ({ body }, res) => {
+      try {
+        const { email, username, password } = body;
+        if ((!email && !username) || !password) {
+          res.status(412).json({ error: true, message: 'Datos incompletos' });
+          return;
+        }
+        const user = await User
+          .findOne({
+            where: {
+              [Op.or]: [
+                { email: email || '' },
+                { username: username || '' },
+              ],
+            },
+          });
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!user || !isPasswordValid) {
+          res.status(404).json({ error: true, message: 'El usuario o contraseña son incorrectos' });
+          return;
+        }
+        delete user.password;
+        // TODO: CHANGE SECRET FOR: process.env.APP_SECRET
+        const token = await jwt.sign({ userId: user.id, role: user.role }, 'temporal-pedro');
+        res
+          .status(200)
+          .json({
+            user,
+            token,
+          });
+      } catch (e) {
+        console.error(e);
         res.status(500).send(e);
       }
     });
